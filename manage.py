0748ac39eb453f29aa2265b89bcda7535b91c573
wangcoo <wangcong@msn.com>
2018-09-26 18:17:03 +0800

    添加项目配置

--- manage.py
+++ manage.py
@@ -1,12 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+
+class Config(object):
+    """项目的配置"""
+    DEBUG = True
+    # 链接数据库
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information777"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# 初始化app对象
 app = Flask(__name__)
+
+# 从对象中加载配置
+app.config.from_object(Config)
+
+# 初始化db数据库对象
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
-    return 'index2222222222'
+    return 'index'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
