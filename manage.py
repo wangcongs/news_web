0b922298734c52be7453115018a37990a8291188
wangcoo <wangcong@msn.com>
2018-09-26 18:29:15 +0800

    集成Redis&开启项目CSRFProtect保护

--- manage.py
+++ manage.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from redis import StrictRedis
 
 
 class Config(object):
     """项目的配置"""
     DEBUG = True
-    # 链接数据库
+    # mysql数据库配置项
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information777"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis数据库配置项
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
 
 
 # 初始化app对象
@@ -18,6 +24,10 @@ app.config.from_object(Config)
 
 # 初始化db数据库对象
 db = SQLAlchemy(app)
+# 创建redis存储对象
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 开启当前项目CSRF保护， 只做服务器验证
+CSRFProtect(app)
 
 
 @app.route('/')
