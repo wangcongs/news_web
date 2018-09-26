d18b3d5a704fbf68251e50c297014c5e698b5d2a
wangcoo <wangcong@msn.com>
2018-09-26 19:42:37 +0800

    session保存位置指定&测试session是否已经设置成功

--- manage.py
+++ manage.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
+from flask_session import Session
 
 
 class Config(object):
     """项目的配置"""
     DEBUG = True
+    SECRET_KEY = '8Q63yvFuBbhjXIRQc8H96Yr0LvGXPwmthzalCoQUNto9J9cTD+ChKxb7Yk3RcvzlmIfZvUWPAxPeIg/J/1aV6A=='
     # mysql数据库配置项
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information777"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -14,6 +16,17 @@ class Config(object):
     # redis数据库配置项
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
+
+    # 指定session的存储位置
+    SESSION_TYPE = "redis"
+    # 开启session签名
+    SESSION_USE_SIGNER = True
+    # 指定存储在哪一个redis中
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置需要过期
+    SESSION_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 
 # 初始化app对象
@@ -26,12 +39,15 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 # 创建redis存储对象
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-# 开启当前项目CSRF保护， 只做服务器验证
+# 开启当前项目CSRF保护， CSRFProtect只做验证工作，cookie中的 csrf_token 和表单中的 csrf_token 需要我们自己实现
 CSRFProtect(app)
+# 为app添加session存储位置Session类的作用，就是为为app指定session的存储位置的
+Session(app)
 
 
 @app.route('/')
 def index():
+    session["name"] = "python"
     return 'index'
 
 
