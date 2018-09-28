118ead7759e79f48dbd9997b416fb454ef5ac861
wangcoo <wangcong@msn.com>
2018-09-28 18:56:10 +0800

    根路径的摸板渲染&并将index.html主页模板移到app创建同级的templates文件下

--- info/modules/index/view.py
+++ info/modules/index/view.py
@@ -1,9 +1,12 @@
+from flask import render_template
+
 from . import index_blu
-from info import redis_store
+# from info import redis_store
 
 
 @index_blu.route("/")
 def index():
     # 在redis设置一个数据，name : wangwu
-    redis_store.set("name", "wangwu")
-    return 'index'
+    # redis_store.set("name", "wangwu")
+    # render_template 会从app创建文件所在的同级templates下找静态文件
+    return render_template("news/index.html")
