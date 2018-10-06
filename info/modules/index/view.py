c9f05638c6ee9ad4dcf93f217b445907fa767509
wangcoo <wangcong@msn.com>
2018-10-06 23:46:54 +0800

    用户是否登录右上角显示&处理登录前端代码bug

--- info/modules/index/view.py
+++ info/modules/index/view.py
@@ -1,5 +1,6 @@
-from flask import render_template, current_app
+from flask import render_template, current_app, session
 
+from info.models import User
 from . import index_blu
 # from info import redis_store
 
@@ -9,7 +10,24 @@ def index():
     # 在redis设置一个数据，name : wangwu
     # redis_store.set("name", "wangwu")
     # render_template 会从app创建文件所在的同级templates下找静态文件
-    return render_template("news/index.html")
+    """
+    显示首页
+    如果用户已经登录，就将登录的信息传递到模板中，供模板使用
+    :return:
+    """
+    # 从session中取到用户ID
+    user_id = session.get("user_id", None)
+    user = None
+    if user_id:
+        # 尝试从数据库中取出当前用户
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+    data = {
+        "user_info": user.to_dict() if user else None
+    }
+    return render_template("news/index.html", data=data)
 
 
 # 访问网站时，浏览器会自动请求根路径 + favicon.ico ，将请求到的文件作为标签的图标,如果不设置就会返回404
