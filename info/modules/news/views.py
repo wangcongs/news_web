1f299ba4fcf4222c54e892518306996bd4a78037
wangcoo <wangcong@msn.com>
2018-10-10 16:40:40 +0800

    收藏和取消收藏的前端 和 后端逻辑实现

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,10 +1,67 @@
-from flask import render_template, request, session, current_app, g, abort
+from flask import render_template, request, session, current_app, g, abort, jsonify
 
 from info import constants
 from info.models import User, News
 # from info.utils.common import user_login_data
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 from . import news_blu
+
+
+@news_blu.route('/news_collect', methods=["POST"])
+@user_login_data
+def news_collect():
+    """
+    收藏新闻(取消收藏逻辑和收藏逻辑类似，也使用这个视图函数，只需要传递过来一个action就可以)
+    1. 接受参数
+    2. 判断参数
+    3. 查询新闻，并判断新闻是否存在
+    :return:
+    """
+
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    # 1. 接受参数
+    news_id = request.json.get("news_id")
+    action = request.json.get("action")
+
+    # 2. 判断参数
+    if not all([news_id, action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ["collect", "cancel_collect"]:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news_id = int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 3. 查询新闻，并判断新闻是否存在
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="未查询到新闻数据")
+
+    # 4. 收藏以及取消收藏
+    if action == "cancel_collect":
+        # 取消收藏
+        if news in user.collection_news:
+            user.collection_news.remove(news)
+    else:
+        # 收藏
+        if news not in user.collection_news:
+            # 添加到用户的新闻收藏列表
+            user.collection_news.append(news)
+
+    return jsonify(errno=RET.OK, errmsg="操作成功")
 
 
 @news_blu.route("/<int:news_id>")
